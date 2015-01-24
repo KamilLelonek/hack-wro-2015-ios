12be73f08ad53ceec0512853b44d9a9c54faa505
Chaatonik <p.charatonik@gmail.com>
2015-01-24 12:37:59 +0100

    Model added

---
+++ TwojaInicjatywa/TwojaInicjatywa/Idea.swift
@@ -0,0 +1,18 @@
+//
+//  Idea.swift
+//  TwojaInicjatywa
+//
+//  Created by Pumpkin K on 24.01.2015.
+//  Copyright (c) 2015 Cohesiva. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Idea{
+    var coordinates : (latitude: Double, attitude: Double)?
+    var comments: [String]?
+    var rank = (pro : 0, con : 0)
+    var photo : UIImage?
+    
+}
