af6754b96f598b6be03d546abdcfc58fc9cd7be7
Michał Ciesielski <ciessielski@gmail.com>
2015-01-24 13:58:30 +0100

    map view adjusted to user's location

--- TwojaInicjatywa/TwojaInicjatywa/View Controllers/MapViewController.swift
+++ TwojaInicjatywa/TwojaInicjatywa/View Controllers/MapViewController.swift
@@ -7,27 +7,67 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 let MapToDetailsSegueIdentifier = "MapToDetails"
 let MapToAddInitiativeSegueIdentifier = "MapToAddInitiative"
 
-class MapViewController: UIViewController
+class MapViewController: UIViewController, CLLocationManagerDelegate
 {
     
     @IBOutlet weak var mapView: GMSMapView!
+    let locationManager = CLLocationManager()
+
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
         var camera = GMSCameraPosition.cameraWithLatitude(51.09168, longitude:17.04015, zoom:14)
-        mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
+        mapView.camera = camera
+        
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled()
+        {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
         
     }
     
-    override func didReceiveMemoryWarning() {
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus)
+    {
+        if status == .AuthorizedWhenInUse
+        {
+            
+            locationManager.startUpdatingLocation()
+            
+            mapView.myLocationEnabled = true
+            mapView.settings.myLocationButton = true
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!)
+    {
+        var locValue:CLLocationCoordinate2D = manager.location.coordinate
+        println("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+        if let location = locations.first as? CLLocation
+        {
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            locationManager.stopUpdatingLocation()
+        }
+    }
+
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
