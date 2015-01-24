b19e9e04d64782d00d3baed00b81577587b1c2c3
Chaatonik <p.charatonik@gmail.com>
2015-01-24 13:41:51 +0100

    addIdeaVC modified

--- TwojaInicjatywa/TwojaInicjatywa/View Controllers/InitiativeAddViewController.swift
+++ TwojaInicjatywa/TwojaInicjatywa/View Controllers/InitiativeAddViewController.swift
@@ -9,5 +9,13 @@
 import UIKit
 
 class InitiativeAddViewController: UIViewController {
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        newIdea: Idea
+        
+    }
 }
