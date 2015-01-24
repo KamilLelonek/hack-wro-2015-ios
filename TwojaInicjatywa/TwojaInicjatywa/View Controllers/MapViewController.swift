91724c08a3ad514bc644ccdb10252f97c592039f
Michał Ciesielski <ciessielski@gmail.com>
2015-01-24 14:44:48 +0100

    sample markers added to mapVC

--- TwojaInicjatywa/TwojaInicjatywa/View Controllers/MapViewController.swift
+++ TwojaInicjatywa/TwojaInicjatywa/View Controllers/MapViewController.swift
@@ -18,16 +18,34 @@ class MapViewController: UIViewController, CLLocationManagerDelegate
     
     @IBOutlet weak var mapView: GMSMapView!
     let locationManager = CLLocationManager()
-
+    var ideas: [Idea] = []
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
+        var idea1 = Idea(coordinates: CLLocationCoordinate2DMake(51.1220568429801, 17.0339102902900), name: "zbudować kościół")
+        var idea2 = Idea(coordinates: CLLocationCoordinate2DMake(51.1220568429222, 17.0339902903400), name: "postawić burdel")
+        
+        ideas.append(idea1)
+        ideas.append(idea2)
+
+        println(ideas)
+        
         var camera = GMSCameraPosition.cameraWithLatitude(51.09168, longitude:17.04015, zoom:14)
         mapView.camera = camera
         mapView.settings.myLocationButton = true
         mapView.myLocationEnabled = true
+        
+        for idea in ideas
+        {
+            var marker = GMSMarker(position: idea.coordinates)
+            marker.title = idea.name
+            marker.map = mapView
+        }
+        
+        
         
         self.locationManager.requestAlwaysAuthorization()
         self.locationManager.requestWhenInUseAuthorization()
